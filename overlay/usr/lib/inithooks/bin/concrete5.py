2ad0bd74d612b013b10c881b72eb10c8e1e0a63c
Stefan Davis <stefan@turnkeylinux.org>
2020-02-12 12:44:56 +1100

    Updated Concrete5
    - upgraded concrete5 to 8.5.2
    - implement workaround for https://github.com/turnkeylinux/tracker/issues/1388

--- overlay/usr/lib/inithooks/bin/concrete5.py
+++ overlay/usr/lib/inithooks/bin/concrete5.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """Set Concrete5 admin password and email
 
 Option:
@@ -17,16 +17,16 @@ from mysqlconf import MySQL
 
 def usage(s=None):
     if s:
-        print >> sys.stderr, "Error:", s
-    print >> sys.stderr, "Syntax: %s [options]" % sys.argv[0]
-    print >> sys.stderr, __doc__
+        print("Error:", s, file=sys.stderr)
+    print("Syntax: %s [options]" % sys.argv[0], file=sys.stderr)
+    print(__doc__, file=sys.stderr)
     sys.exit(1)
 
 def main():
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
                                        ['help', 'pass=', 'email='])
-    except getopt.GetoptError, e:
+    except getopt.GetoptError as e:
         usage(e)
 
     password = ""
@@ -57,7 +57,8 @@ def main():
     inithooks_cache.write('APP_EMAIL', email)
 
     salt = bcrypt.gensalt()
-    hashpass = bcrypt.hashpw(password, salt)
+    hashpass = bcrypt.hashpw(password.encode('utf8'), salt).decode('utf8')
+    print('hashpass: %s' % hashpass)
 
     m = MySQL()
     m.execute('UPDATE concrete5.Users SET uPassword=\"%s\" WHERE uName=\"admin\";' % hashpass)
